2192a1909b21573c7e61696ec158aaca2c90ab5f
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2025-01-21 13:22:04 -0700

    add to cart works
    add function works as well as get the list of stored values

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,28 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+// This get the items in the local storage for what ever was in the cart already
+function getCart(key) {
+  let retrievedList = []
+  const storedListString = localStorage.getItem(key);
+  console.log(storedListString)
+  // checks to make sure there cart is not empty if so we return a empty cart
+  if (storedListString != null) {
+    retrievedList = JSON.parse(storedListString);
+    console.log(retrievedList)
+  }
+  else {
+    console.log("no items in the cart")
+  }
+  //returns any items in the cart that were in localstorage
+  return retrievedList
+}
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let retrievedList = getCart("so-cart")
+  // Add the product to the list of products in the cart
+  retrievedList.push(product)
+  // Save the new list to local storage and overwrite the old one
+  setLocalStorage("so-cart", retrievedList);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
