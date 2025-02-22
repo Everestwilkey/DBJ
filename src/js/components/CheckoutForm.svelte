305b58bee2d692685e2d15db5060d194f534c127
camwoodward3 <camwoodward3@icloud.com>
2025-02-22 16:21:21 -0700

    added stuff regarding the Checkout stufff

---
+++ src/js/components/CheckoutForm.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { getLocalStorage } from "../utils.mjs";
+    import { onMount } from "svelte";
+// props
+    let {key = ""} = $props();
+
+    // state var
+    let list = $state([]);
+    let itemTotal = $state(0);
+    let shipping = $state(0);
+    let tax = $state(0);
+    let orderTotal = $state(0);
+
+
+    const init = function() {
+        list = getLocalStorage(key);
+        calculateSummary();
+    };
+
+    const calculateSummary = function() {
+        const amounts = list.map((item) => item.FinalPrice);
+        itemTotal = amounts.reduce((sum, item) => sum + item);
+    };
+    const calculateOrdertotal = function() {
+        shipping = 10 + (list.length - 1) * 2;
+        tax = (itemTotal * 0.06).toFixed(2);
+        orderTotal = (
+            parseFloat(itemTotal) +
+            parseFloat(shipping) +
+            parseFloat(tax)
+        ).toFixed(2);
+    };
+
+    // initial setup
+    onMount(init);
+</script>
+
+<fieldset class="checkout-summary">
+    <legend>Order Summary</legend>
+    
+    <p>${tax}</p>
+    
+</fieldset>
+
